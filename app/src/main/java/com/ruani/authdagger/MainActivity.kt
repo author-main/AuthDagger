d14ed3a3fedbba5b063b20a715fc3b4bfe80100f
iUser <author-main@mail.ru>
2021-08-05 10:57:16 +0300

    05.08.2021

--- app/src/main/java/com/ruani/authdagger/MainActivity.kt
+++ app/src/main/java/com/ruani/authdagger/MainActivity.kt
@@ -5,12 +5,14 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatDelegate
 
 class MainActivity : AppCompatActivity() {
+    private fun setNightMode(){
+        AppCompatDelegate.setDefaultNightMode(
+            AppCompatDelegate.MODE_NIGHT_YES
+        )
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         supportActionBar?.hide()
-        AppCompatDelegate.setDefaultNightMode(
-            AppCompatDelegate.MODE_NIGHT_YES
-        )
     }
 }
