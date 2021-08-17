3e99bc70c505a813cd8b99af4b923fa6b9ae5148
iUser <author-main@mail.ru>
2021-08-17 13:18:57 +0300

    17.08.2021

--- app/src/main/java/com/ruani/authdagger/mvp/model_classes/CipherPassword.kt
+++ app/src/main/java/com/ruani/authdagger/mvp/model_classes/CipherPassword.kt
@@ -2,6 +2,7 @@ package com.ruani.authdagger.mvp.model_classes
 
 import com.ruani.authdagger.getAppContext
 import com.ruani.authdagger.interfaces.IOCipherPassword
+import java.security.KeyStore
 
 class CipherPassword: IOCipherPassword {
     private val providerKeyStore: String = "AndroidKeyStore"
@@ -21,5 +22,28 @@ class CipherPassword: IOCipherPassword {
 
     override fun correctPassword(valie: String): Boolean {
         TODO("Not yet implemented")
+    }
+
+    private fun initKeys(): Boolean{
+        val keyStore = getKeyStore() ?: return false
+        return try {
+            val privateKey = keyStore.getKey(alias, null)
+            val certificate = keyStore.getCertificate(alias)
+            privateKey !=null && certificate?.publicKey != null
+        } catch (e: Exception){
+            false
+        }
+    }
+
+    private fun getKeyStore(): KeyStore?{
+        var keyStore: KeyStore? = null
+        return try{
+            keyStore = KeyStore.getInstance(providerKeyStore)
+            keyStore?.load(null)
+            keyStore
+        } catch (e: Exception) {
+            keyStore?.deleteEntry(alias)
+            null
+        }
     }
 }
