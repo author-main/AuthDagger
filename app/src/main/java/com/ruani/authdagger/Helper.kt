7641e7be108e5500ae895077057cf06c3c42992c
iUser <author-main@mail.ru>
2021-08-24 13:30:49 +0300

    24.08.2021

--- app/src/main/java/com/ruani/authdagger/Helper.kt
+++ app/src/main/java/com/ruani/authdagger/Helper.kt
@@ -1,14 +1,26 @@
 package com.ruani.authdagger
 
+import android.graphics.Color
 import android.util.Log
+import android.util.TypedValue
+import androidx.core.content.ContextCompat
 
 const val FILE_PREFERENCES = "settings"
 const val KEY_MAIL = "key_mail"
 const val KEY_PASSWORD = "key_password"
+const val DEFAULT_COLOR = Color.LTGRAY
 
 fun getAppContext() = AuthApplication.applicationContext()
 fun log(message: String?){
     if (message.isNullOrBlank())
         return
     Log.v("authdagger", message)
+}
+
+fun getColorFromTheme(id: Int): Int {
+    val typedValue = TypedValue()
+    return if (getAppContext().theme.resolveAttribute(id, typedValue, true))
+        typedValue.data
+    else
+        DEFAULT_COLOR
 }
--- app/src/main/java/com/ruani/authdagger/mvp/presenter_classes/viewPasswordHelper.kt
+++ app/src/main/java/com/ruani/authdagger/mvp/presenter_classes/viewPasswordHelper.kt
@@ -1,20 +1,44 @@
 package com.ruani.authdagger.mvp.presenter_classes
 
 import android.widget.TextView
+import com.ruani.authdagger.R
+import com.ruani.authdagger.getAppContext
+import com.ruani.authdagger.getColorFromTheme
 
 class viewPasswordHelper(private val symbols: Array<TextView>) {
-    var onChangePassword: ((password: String) -> Unit)? = null
-    fun changeData(index: Int, value: String?){
+    private val authFinger = "FINGER"
+    var onChangePassword: (() -> Unit)? = null
+    private val colorSymbol = getColorFromTheme(android.R.attr.textColorSecondary)
+    private val colorSymbolActive = getColorFromTheme(R.attr.colorAccent)
+    val hidenSymbol = "•"
+
+    private fun setSymbolColor(index: Int, active: Boolean = false){
+        val color =
+            if (active)
+                colorSymbolActive
+            else
+                colorSymbol
+        symbols[index].setTextColor(color)
+    }
+
+    fun changeSymbol(index: Int, value: String?){
         if (value.isNullOrBlank())
-            symbols[index].text = ""
-        else
+            setSymbolColor(index)
+        else {
+            if (value == authFinger) {
+                for (i in 0 until symbols.size)
+                    setSymbolColor(i, true)
+                return
+            }
             symbols[index].text = value
-        if (onChangePassword != null && index == 3 && value != null) {
-                var password = ""
-                symbols.forEach {symbol->
-                    password += symbol
-                }
-                onChangePassword?.invoke(password)
+        }
+
+        if (index == 3) {
+            if (value != null && onChangePassword != null){
+                onChangePassword?.invoke()
+            }
+        } else {
+
         }
     }
 }
