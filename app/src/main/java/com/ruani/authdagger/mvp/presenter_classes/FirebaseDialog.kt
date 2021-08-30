4a13cc75d95af56575d46dedb284eb55d9921d8b
iUser <author-main@mail.ru>
2021-08-30 08:28:28 +0300

    30.08.2021

--- app/src/main/java/com/ruani/authdagger/mvp/presenter_classes/FirebaseDialog.kt
+++ app/src/main/java/com/ruani/authdagger/mvp/presenter_classes/FirebaseDialog.kt
@@ -3,12 +3,25 @@ package com.ruani.authdagger.mvp.presenter_classes
 import com.ruani.authdagger.abstract_data.Contract
 import com.ruani.authdagger.interfaces.AuthDialog
 
+const val DIALOG_RESTORE  = 0
+const val DIALOG_REGISTER = 1
+
 class FirebaseDialog<T: Contract.IView>: AuthDialog<T>() {
     override fun showDialogRegister(email: String?) {
-        TODO("Not yet implemented")
+        show(DIALOG_REGISTER, email)
     }
 
     override fun showDialogRestore(email: String?) {
-        TODO("Not yet implemented")
+        show(DIALOG_RESTORE, email)
+    }
+
+    private fun show(value: Int, email: String?){
+        if (email.isNullOrBlank()) {
+            return
+        }
+        if (value == DIALOG_REGISTER) {
+        }
+        if (value == DIALOG_RESTORE) {
+        }
     }
 }
