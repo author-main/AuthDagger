09e4b09799c38a766f6f8a3df361467e115134d4
iUser <author-main@mail.ru>
2021-08-31 13:32:12 +0300

    31.08.2021

--- app/src/main/java/com/ruani/authdagger/mvp/model_classes/FingerPrint.kt
+++ app/src/main/java/com/ruani/authdagger/mvp/model_classes/FingerPrint.kt
@@ -1,18 +1,12 @@
 package com.ruani.authdagger.mvp.model_classes
 
-import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import androidx.biometric.BiometricManager
 import androidx.biometric.BiometricManager.Authenticators.BIOMETRIC_STRONG
 import androidx.biometric.BiometricManager.Authenticators.DEVICE_CREDENTIAL
-import androidx.biometric.BiometricPrompt
-import androidx.core.content.ContextCompat
-import com.ruani.authdagger.R
 import com.ruani.authdagger.abstract_data.Contract
 import com.ruani.authdagger.abstract_data.auth_data
 import com.ruani.authdagger.getAppContext
-import com.ruani.authdagger.getStringResource
-import javax.crypto.Cipher
 
 abstract class FingerPrint<T: Contract.IView> {
     var onAuthBiometricComplete: ((value: auth_data.FingerValue?) -> Unit)? = null
