f8d0ead88452903f5e08cb12a8258de8d963700c
iUser <author-main@mail.ru>
2021-09-01 20:55:13 +0300

    31.08.2021

--- app/src/main/java/com/ruani/authdagger/dagger/AppComponent.kt
+++ app/src/main/java/com/ruani/authdagger/dagger/AppComponent.kt
@@ -2,8 +2,10 @@ package com.ruani.authdagger.dagger
 
 import com.ruani.authdagger.MainActivity
 import dagger.Component
+import javax.inject.Singleton
 
 @Component(modules = [PresenterModule::class])
+@Singleton
 interface AppComponent {
     fun injectsMainActivity(mainActivity: MainActivity)
 }
--- app/src/main/java/com/ruani/authdagger/dagger/PresenterModule.kt
+++ app/src/main/java/com/ruani/authdagger/dagger/PresenterModule.kt
@@ -4,8 +4,10 @@ import com.ruani.authdagger.mvp.Contract
 import com.ruani.authdagger.mvp.Presenter
 import dagger.Module
 import dagger.Provides
+import javax.inject.Singleton
 
 @Module
+@Singleton
 class PresenterModule {
     @Provides
     fun providePresenter(): Presenter<Contract.IView> {
