dac6f2ca414b9b9dfc6a7ba2c96f5a52062ebfbe
iUser <author-main@mail.ru>
2021-09-03 11:05:58 +0300

    03.09.2021

--- app/src/main/java/com/ruani/authdagger/dagger/PresenterModule.kt
+++ app/src/main/java/com/ruani/authdagger/dagger/PresenterModule.kt
@@ -3,11 +3,6 @@ package com.ruani.authdagger.dagger
 import com.ruani.authdagger.mvp.Contract
 import com.ruani.authdagger.mvp.Model
 import com.ruani.authdagger.mvp.Presenter
-import com.ruani.authdagger.mvp.interfaces.AuthServer
-import com.ruani.authdagger.mvp.model_classes.AuthFingerPrint
-import com.ruani.authdagger.mvp.model_classes.FirebaseServer
-import com.ruani.authdagger.mvp.model_classes.UserDataStorage
-import com.ruani.authdagger.mvp.presenter_classes.Dyer
 import com.ruani.authdagger.mvp.presenter_classes.FirebaseDialog
 import dagger.Module
 import dagger.Provides
